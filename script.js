33d9fb05701f14839427cc37e39e8d5ef3ee5fb7
saber46 <sabermahmud.sourav.7@gmail.com>
2022-02-25 02:14:33 +0600

    Update

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 
-const links = document.querySelector('.links');
-const menu = document.querySelector('.menu');
-var isLinkOpen = 0;
+var links = document.querySelector('.links');
+var menu = document.querySelector('.menu');
 
 menu.addEventListener('click', ()=> {
-    if(isLinkOpen == 0) {
-        menu.innerHTML = 'close';
-        links.style.right = '0';
-        isLinkOpen++;
-    }
-    else {
-        menu.innerHTML = 'menu';
-        links.style.right = '-60vw';
-        isLinkOpen--;
-    }
+    links.classList.toggle('active');
+    menu.classList.toggle('active');
 })
